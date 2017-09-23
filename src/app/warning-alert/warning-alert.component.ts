9d5f65c1238f600429dd0004dceba2a03e088925
T S Sanjay Raju <tssanjayraju@Ts-MacBook-Pro.local>
2017-09-24 00:10:23 +1000

    ok

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: '[app-warning-alert]',
+    templateUrl: './warning-alert.component.html',
+    styles: [`
+    span{
+padding: 20px;
+background-color: mistyrose;
+border: 1px solid;
+
+    }
+    `]
+})
+export class WarningAlertComponent{
+
+}
